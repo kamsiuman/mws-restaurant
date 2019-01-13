ce3c15bb4af1bde96fd535de49568afdef07a674
Alex Wong <awong5@slb.com>
2019-01-13 13:07:04 -0600

    implemented the service worker and test locally

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,5 @@
-const cacheFiles = [
+const staticCache = "restaurant-static-v1";
+const staticAssets = [
     '/',
     '/img/1.jpg',
     '/img/2.jpg',
@@ -10,6 +11,7 @@ const cacheFiles = [
     '/img/8.jpg',
     '/img/9.jpg',
     '/img/10.jpg',
+    '/img/restaurant.ico',
     '/index.html',
     '/restaurant.html',
     '/css/styles-index.css',
@@ -20,34 +22,79 @@ const cacheFiles = [
     '/data/restaurants.json'
 ];
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open('v1').then(function (cache) {
-            return cache.addAll(cacheFiles);
+/**
+ * Call install event
+ */
+self.addEventListener("install", event => {
+    console.log('Service Worker : Installed');
+    event.waitUntil(
+        caches.open(staticCache)
+        .then(cache => {
+            console.log('Service Worker : Caching Files');
+            cache.addAll(staticAssets);
         })
+        .catch(error => console.error("Service Worker : Install - Something happened", error))
     );
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            if (response) {
-                console.log('Found ', e.request, ' in cache');
-                return response;
-            } else {
-                console.log('Could not find ', e.request, ' in cache, FETCHING!');
-                return fetch(e.request)
-                    .then(function (response) {
-                        const clonedResponse = response.clone();
-                        caches.open('v1').then(function (cache) {
-                            cache.put(e.request, clonedResponse);
-                        })
-                        return response;
-                    })
-                    .catch(function (err) {
-                        console.error(err);
-                    });
-            }
-        })
+/**
+ * Call activate event : cleanup the old cache here !!!!
+ */
+self.addEventListener("activate", event => {
+    console.log('Service Worker : Activated');
+    //Remove unwanted caches
+    event.waitUntil(
+        caches.keys()
+        .then(cacheNames =>
+            Promise.all(
+                cacheNames.filter(cacheName => cacheName.startsWith("restaurant") &&
+                    !staticCache.includes(cacheName))
+                .map(cacheName => {
+                    console.log('Service Worker : Activated : Deleted old cache => ' + cacheName);
+                    caches.delete(cacheName);
+                })
+            )
+        )
+        .catch(error => console.error("Service Worker : Activate - Something happened", error))
     );
 });
+
+/**
+ * Call fetch event
+ */
+self.addEventListener("fetch", event => {
+    console.log('Service Worker : Fetch');
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)))
+});
+
+
+// const port = event.request.url.split("/")[2].split(":")[1];
+
+// if (event.request.method === "GET" &&
+//     (event.request.url.search("localhost") === -1) || // calling for heroku and google maps api
+//     (port !== undefined && port === "1337") // calling for local backend running on port 1337
+// ) {
+//     event.respondWith(serveResponseIdb(event.request));
+// } else {
+//     event.respondWith(serveResource(event.request));
+// }
+
+
+const serveResource = request =>
+    caches.open(staticCache)
+    .then(cache =>
+        cache.match(request)
+        .then(response => {
+            if (response) {
+                return response;
+            }
+
+            return fetch(request)
+                .then(networkResponse => {
+                    cache.put(request, networkResponse.clone())
+                        .then(() => console.info("Caching succeed"))
+                        .catch(error => console.error("Caching failed", error));
+                    return networkResponse;
+                });
+        }))
+    .catch(error => console.error("Something happened", error));
